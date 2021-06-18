b5e497aa130f58328735ddd6250d3a9adddd7080
shahalnishan <30800292+shahalnishan@users.noreply.github.com>
2021-06-18 23:45:56 +0530

    Update student.js

--- js/student.js
+++ js/student.js
@@ -88,10 +88,10 @@ function videotoggle(){
 function videoicontoggle(){
     if(camtoggle){
         var videobtnfont = document.getElementById("videobtn-font")
-        videobtnfont.setAttribute("class","fas fa-video")
+        videobtnfont.setAttribute("class","fas fa-video-slash")
     }
     else{
         var videobtnfont = document.getElementById("videobtn-font")
-        videobtnfont.setAttribute("class","fas fa-video-slash")
+        videobtnfont.setAttribute("class","fas fa-video")
     }
 }
